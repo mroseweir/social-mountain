f6fd6f324e5e2bff27497896ec800f258b6f3cb9
mroseweir <roseweirmichael@gmail.com>
2021-08-16 14:14:44 -0500

    solves first problem

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import Post from "./Post/Post";
 
-import './App.css';
+import "./App.css";
 
-import Header from './Header/Header';
-import Compose from './Compose/Compose';
+import Header from "./Header/Header";
+import Compose from "./Compose/Compose";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      posts: []
+      posts: [],
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
   }
-  
+
   componentDidMount() {
-
+    axios.get("https://practiceapi.devmountain.com/api/posts").then((res) => {
+      this.setState({ posts: res.data });
+    });
   }
 
-  updatePost() {
-  
-  }
+  updatePost() {}
 
-  deletePost() {
+  deletePost() {}
 
-  }
-
-  createPost() {
-
-  }
+  createPost() {}
 
   render() {
     const { posts } = this.state;
@@ -42,9 +40,10 @@ class App extends Component {
         <Header />
 
         <section className="App__content">
-
           <Compose />
-          
+          {posts.map((post) => (
+            <Post key={post.id} />
+          ))}
         </section>
       </div>
     );
